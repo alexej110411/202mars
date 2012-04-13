269e829bc25fbe76b1def1b3ee16a3e1749984e4
renishd600@gmail.com <renishd600@gmail.com@f98c1ea7-37c5-d207-0bf3-b743c99eeef7>
2012-04-13 23:50:36 +0000

    KeyPad and PassCode commented.

--- 202mars/Starbucks/src/com/sb/view/PinScreen.java
+++ 202mars/Starbucks/src/com/sb/view/PinScreen.java
@@ -1,4 +1,5 @@
 package com.sb.view;
+
 import com.sb.controller.AppController;
 import com.sb.controller.ScreenState;
 
@@ -6,11 +7,11 @@ import processing.core.PApplet;
 import processing.core.PFont;
 import processing.core.PImage;
 
-public class PinScreen implements ScreenState{
-	
+public class PinScreen implements ScreenState {
+
 	AppController appController;
-	KeyPad keypad = new KeyPad();
-	PassCode passcode = new PassCode();
+	// KeyPad keypad = new KeyPad();
+	// PassCode passcode = new PassCode();
 
 	PImage img;
 	PFont f;
@@ -21,18 +22,15 @@ public class PinScreen implements ScreenState{
 	}
 
 	public void setup(PApplet applet) {
-		keypad.draw(applet);
-		passcode.draw(applet);
-		
+		// keypad.draw(applet);
+		// passcode.draw(applet);
 
 	}
 
-	
 	public void mousePressed(PApplet applet) {
 		if (applet.mouseX > 0 && applet.mouseX < 88 && applet.mouseY > 240
 				&& applet.mouseY < 280) {
 
-			
 		}
 
 	}
@@ -40,13 +38,13 @@ public class PinScreen implements ScreenState{
 	@Override
 	public void draw(PApplet applet) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void drawArrow(int cx, int cy, int len, float angle, PApplet applet) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
